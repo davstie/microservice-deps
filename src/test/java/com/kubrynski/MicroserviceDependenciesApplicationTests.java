13b51df345e4625ece4ac8cd3b284d812d6ddee6
Jakub Kubrynski <jkubrynski@gmail.com>
2016-09-23 15:21:22 +0200

    Bootstrap

---
+++ src/test/java/com/kubrynski/MicroserviceDependenciesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kubrynski;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceDependenciesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
